a01867034bdc3fdfaf3183827a31d19639cbc724
dylansmallms@gmail.com <dylansmallms@gmail.com>
2020-02-02 22:09:45 +0200

    Initial sync

---
+++ PetersPapers/Models/ViewModels/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetersPapers.Models.ViewModels
+{
+    public class UserModel
+    {
+        public User user { get; set; }
+        public UserModel(int Id)
+        {
+            using (PetersPapers_TestEntities db = new PetersPapers_TestEntities())
+            {
+                this.user = db.Users.Where(x => x.Id == Id).FirstOrDefault();
+            }
+        }
+        public UserModel(string UserName)
+        {
+            using (PetersPapers_TestEntities db = new PetersPapers_TestEntities())
+            {
+                this.user = db.Users.Where(x => x.UserName == UserName).FirstOrDefault();
+            }
+        }
+
+    }
+}
